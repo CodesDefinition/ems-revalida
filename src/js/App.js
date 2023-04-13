a6dfbd95e4fbcdb62e705691f64572b9aeffe278
CPANEN@win-wks301522 <christina.micah.cm@gmail.com>
2023-04-13 12:03:56 +0800

    Employee Front-end added

--- src/js/App.js
+++ src/js/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Appbar from "./Header";
 import Homepage from "./admin/Homepage";
 import About from "./About";
+import Employee from "./admin/Employee";
 const App = () => {
   return (
     <React.Fragment>
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<About />} />
+          <Route path="/admin/employee" element={<Employee />} />
         </Routes>
       </div>
     </React.Fragment>
